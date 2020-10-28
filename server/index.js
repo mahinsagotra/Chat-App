dcd51250a8aab4d12759ab10144dc9c063f4a8cb
Mahin Sagotra <mahinsagotra18@gmail.com>
2020-10-28 19:56:49 +0530

    Add Join Component

--- server/index.js
+++ server/index.js
@@ -9,12 +9,21 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    // below are engine.IO options
+    pingInterval: 10000,
+    pingTimeout: 5000,
+});
 
 app.use(router);
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+        callback();
+    })
 
     socket.on('disconnect', () => {
         console.log('User has left');
