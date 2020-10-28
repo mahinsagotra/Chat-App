c6ee83a22131b81e5502248672bfd364f1d45b76
Mahin Sagotra <mahinsagotra18@gmail.com>
2020-10-28 20:55:17 +0530

    Add Join and sendMessage features

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketio = require('socket.io');
 const http = require('http');
 const { listeners } = require('process');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./user.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -18,11 +20,23 @@ const io = socketio(server, {
 app.use(router);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: '${user.name}, Welcome to the room ${user.room}' });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: '${user.name} has joined!' });
+
+        socket.join(user.room);
+
         callback();
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
     })
 
     socket.on('disconnect', () => {
